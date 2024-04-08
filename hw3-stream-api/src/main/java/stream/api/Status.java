ed90e014946dce110bb3df0a0b841939bf30201b
Daniil Antonov <daniilantonov406@gmail.com>
2024-04-08 13:43:34 +0300

    Added lombok for enum

--- hw3-stream-api/src/main/java/stream/api/Status.java
+++ hw3-stream-api/src/main/java/stream/api/Status.java
@@ -1,15 +1,13 @@
 package stream.api;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Getter
 public enum Status {
     OPEN("открыта"),
     AT_WORK("в работе"),
     CLOSED("закрыта");
     private final String status;
-
-    Status(String status) {
-        this.status = status;
-    }
-    public String getStatus() {
-        return status;
-    }
 }
